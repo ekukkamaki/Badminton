8b300b8ebd26db7ff3097f2a4ad6a9a9db91899a
Esa <ekukkamaki@gmail.com>
2017-02-13 22:29:56 +0200

    webbi serveri debuggausta varten visual studiossa

---
+++ server.ts
@@ -0,0 +1,6 @@
+﻿import http = require('http');
+import server = require('./src/server');
+var port = process.env.port || 1337
+var app = server.Server.bootstrap().app;
+
+http.createServer(app).listen(port);
